4dd6ac60e7ec67609ba8613777548c5960c5d1e5
siddhant0912 <siddhantarekar12@gmail.com>
2020-02-10 20:21:16 +0530

    recipe view added

---
+++ src/js/models/Recipe.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import { elements } from '../views/base';
+
+export default class recipe {
+    constructor(id) {
+        this.id = id;
+    }
+
+    async getRecipe() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    calcTime() {
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.servings = 2;
+    }
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitShort, 'kg', 'g'];
+        const newIngredients = this.ingredients.map(el => {
+            //uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitShort[i]);
+            });
+            //remove parenthsis
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            //parse ingredients into count, unit and ingredients
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
+
+            let objIng;
+            if (unitIndex > -1) {
+                //unit p
+                let count;
+                const arrCount = arrIng.slice(0, unitIndex);
+                if (arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-', '+'));
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));
+                }
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                };
+
+            } else if (unitIndex === -1) {
+                //unit a
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            } else if (parseInt(arrIng[0], 10)) {
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+            }
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
+    updateServings(type) {
+        //servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+
+
+        //ingredients
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.servings);
+        });
+
+        this.servings = newServings;
+
+    }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import Search from './models/Search';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchview from './views/searchView';
+import * as RecipeView from './views/RecipeView';
+import Recipe from './models/Recipe'
 /*
 Global Object
 -Search Object
@@ -24,13 +26,17 @@ const controlSearch = async() => {
     renderLoader(elements.searchRes);
 
 
+    try {
+        //4.Search for Recipes
+        await state.search.getResults();
 
-    //4.Search for Recipes
-    await state.search.getResults();
-
-    //5.Render results on ui
-    clearLoader();
-    searchview.renderResults(state.search.result);
+        //5.Render results on ui
+        clearLoader();
+        searchview.renderResults(state.search.result);
+    } catch (err) {
+        alert(err);
+        clearLoader();
+    }
 }
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -44,6 +50,65 @@ elements.searchResPages.addEventListener('click', e => {
         const goToPage = parseInt(btn.dataset.goto, 10);
         searchview.clearResults();
         searchview.renderResults(state.search.result, goToPage);
+    }
+
+});
+
+//Recipe Controller
+const controlRecipe = async() => {
+
+    //get id from url
+    const id = window.location.hash.replace('#', '');
+    if (id) {
+        //prepare ui
+        RecipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        //highlight selected seach
+        if (state.search) {
+            searchview.highlightedSelected(id);
+        }
+        //create new recipe object
+        state.recipe = new Recipe(id);
+
+        ;
+        try {
+            //get recipe data
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            //calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //render recipe
+            clearLoader();
+            RecipeView.renderRecipe(state.recipe);
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+};
+
+
+window.addEventListener('hashchange', controlRecipe);
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//recipe button click
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            //decrease button is clicked
+            state.recipe.updateServings('dec');
+            RecipeView.updateServingsIng(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        //decrease button is clicked
+        state.recipe.updateServings('inc');
+        RecipeView.updateServingsIng(state.recipe);
     }
 
 });
